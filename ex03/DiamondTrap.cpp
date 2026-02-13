a114fa5f46576463477bde271544658b69831b88
bkaras-g <bkaras-g@student.42.fr>
2026-02-13 14:35:00 +0100

    add DT::operator=()

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 13:01:19 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/02/13 13:27:08 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/02/13 14:34:38 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,16 @@ DT::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name")
 	this->_hit_points = FragTrap::_hit_points;
 	this->_energy_points = ScavTrap::_energy_points;
 	this->_attack_damage = FragTrap::_attack_damage;
+}
+
+DiamondTrap    &DT::operator=(const DiamondTrap &trap)
+{
+	std::cout << "DiamondTrap copy assignment operator called" << std::endl;
+    if (this != &trap)
+    {
+        this->ClapTrap::operator=(trap);
+		this->_name = trap._name;
+    }
+    return (*this);
 }
 
