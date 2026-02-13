1b1206cb3bf8b07496384b14929dd3a435eb0e83
bkaras-g <bkaras-g@student.42.fr>
2026-02-13 13:27:27 +0100

    add DiamondTrap subject constructor

--- ex03/DiamondTrap.cpp
+++ ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 13:01:19 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/02/13 13:22:28 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/02/13 13:27:08 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,11 @@ DT::DiamondTrap() : ClapTrap(), _name("default")
 	this->ClapTrap::_name += "_clap_name";
 }
 
-DT::DiamondTrap(std::string name) : ClapTrap(name)
+DT::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name")
 {
 	std::cout << "DiamondTrap subject constructor called" << std::endl;
-
+	this->_hit_points = FragTrap::_hit_points;
+	this->_energy_points = ScavTrap::_energy_points;
+	this->_attack_damage = FragTrap::_attack_damage;
 }
 
